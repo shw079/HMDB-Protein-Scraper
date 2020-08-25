ccc833cd69d6452699d4f8bc03a13998a1240b32
shw079 <shuow@princeton.edu>
2020-08-24 20:23:11 -0400

    Change output to tab-delimited

--- scraper.py
+++ scraper.py
@@ -3,11 +3,11 @@ import argparse
 from src.pquery import ProteinQuery
 
 
-def write_csv(records, out):
+def write_tsv(records, out):
     with open(out, "w") as h:
         h.write("HMDB_id,UniProt_id,name\n")
         for record in records:
-            h.write(",".join(record))
+            h.write("\t".join(record))
             h.write("\n")
 
 
@@ -16,10 +16,10 @@ if __name__ == "__main__":
     parser.add_argument("-k", type=str, required=True,
                         help="keywords to search in HMDB")
     parser.add_argument("-o", type=str, required=True,
-                        help="output path of comma-delimited result table")
+                        help="output path of tab-delimited result table")
     args = parser.parse_args()
 
     query = ProteinQuery(args.k)
     query.parse()
-    write_csv(query.res, args.o)
+    write_tsv(query.res, args.o)
     print("---Found {} records of {}---".format(len(query.res), args.k))
